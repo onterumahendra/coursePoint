66194cc74b7262bca55ad5328ab0382553465136
onterumahendra <msbrother445@gmail.com>
2018-05-08 21:45:45 +0530

    login form validation added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -38,6 +39,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class loginComponent {
-  title = 'Course Point';
+  
+  loginForm: FormGroup;
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.loginForm = new FormGroup({
+      // tslint:disable-next-line
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  public login() {
+    console.log(this.loginForm.value);
+  }
 }
